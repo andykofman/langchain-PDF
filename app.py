3bc7b9e38f292a34dd13a05c1b7ddd0dfb5bb9a4
Ahmed Ali <ali.a@aucegypt.edu>
2025-07-08 08:54:32 +0300

    minor fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chains.question_answering import load_qa_chain
